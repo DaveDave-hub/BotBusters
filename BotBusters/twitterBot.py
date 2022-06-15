729fbf7064488e82da5b30a3b3f3031cbffb21c3
PaulinaW <76110024+paulina-iit@users.noreply.github.com>
2022-06-15 11:35:04 +0200

    update

--- BotBusters/twitterBot.py
+++ BotBusters/twitterBot.py
@@ -21,6 +21,7 @@ api = tweepy.API(auth)
 #getting factors for account after providing username
 def get_factors(screen_name):
     try:
+        #here we get username (for checking numbers in the name)
         user = api.get_user(screen_name)
         print(user)
 
@@ -33,9 +34,39 @@ def get_factors(screen_name):
         tweets_number = user.statuses_count
         #tweets per day - average
         tweets_per_day = round(tweets_number / account_age, 2)
-        #we still need number of retweets, languages and hours of posting
+        #language, posting time, retweets
+        posting_time = int(user.created_at.strftime("%H")) #strftime means date to string, we need hour only
 
-    except
+        # list of factors
+        account_factors =[verified_acocunt, account_age, followers_number, friend_number, tweets_per_day, posting_time]
+
+        #if else statements to check conditions
+
+        true_conditions = 0
+        if verified_acocunt == True
+            return 'Not a bot'
+
+        if verified_acocunt == False:
+            if tweets_per_day > 100:
+                true_conditions += 1
+
+            if account_age <= 365:
+                true_conditions += 1
+
+            if friend_number <= 10 and followers_number >= 10000:
+                true_conditions += 1
+
+    except:
         return 'User not found'
 
+def bot_prediction(twitter_username):
+
+    user_factors = get_factors(twitter_username)
+    true_conditions = 0
+
+    if user_factors =='User not found':
+        return 'User not found'
+
+    else:
+        factors =['verfied-account', 'account age', 'followers number', 'friends number', 'tweets per day', 'posting time']
 
